2d1a5ddaed471819120293f7a8df9388a8bdcad6
jeounpar <cdex6531@ajou.ac.kr>
2022-09-22 20:20:06 +0900

    add Params Query Body

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
 
 @Controller('student')
@@ -13,6 +22,36 @@ export class StudentController {
   @Post()
   createStudent() {
     return this.studentService.createStudent('Park', '201720799');
+  }
+
+  @Post('/params/:name/:studentId')
+  createStudentWithParams(
+    @Param('name') name: string,
+    @Param('studentId') studentId: string,
+  ) {
+    console.log(name);
+    console.log(studentId);
+    return this.studentService.createStudent(name, studentId);
+  }
+
+  @Post('/body')
+  createStudentWithBody(
+    @Body('name') name: string,
+    @Body('studentId') studentId: string,
+  ) {
+    console.log(name);
+    console.log(studentId);
+    return this.studentService.createStudent(name, studentId);
+  }
+
+  @Post('/query')
+  createStudentWithQuery(
+    @Query('name') name: string,
+    @Query('studentId') studentId: string,
+  ) {
+    console.log(name);
+    console.log(studentId);
+    return this.studentService.createStudent(name, studentId);
   }
 
   @Delete()
